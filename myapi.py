24aeb579c620ad920f5728703157589ed38951a4
Edu-0 <164441707+Edu-0@users.noreply.github.com>
2026-02-24 01:47:00 -0300

    Feature: New functions and basic commands covered

--- myapi.py
+++ myapi.py
@@ -1,10 +1,13 @@
+# https://www.youtube.com/watch?v=tLKKmouUams - Video used to learn API
+
 from fastapi import FastAPI, Path # This object of FastAPI will be used later to create our APIs
 from typing import Optional # It's just a good practice to add on Query Parameters, that way the endpoint stays valid even without a parameter
+from pydantic import BaseModel
 
 app = FastAPI() # The FastAPI object has many attributes and will be used
 
 students = { # Just generated some with AI to test with
-    1: {"name": "Eduardo", "age": 19, "grade": 12, "email": "eduardo@email.com"},
+    1: {"name": "Felix", "age": 17, "grade": 12, "email": "eduardo@email.com"},
     2: {"name": "Mariana", "age": 18, "grade": 12, "email": "mariana@email.com"},
     3: {"name": "Lucas", "age": 17, "grade": 11, "email": "lucas@email.com"},
     4: {"name": "Ana", "age": 18, "grade": 12, "email": "ana@email.com"},
@@ -16,6 +19,18 @@ students = { # Just generated some with AI to test with
     10: {"name": "Isabela", "age": 17, "grade": 11, "email": "isabela@email.com"},
     11: {"name": "Eduardo", "age": 15, "grade": 5, "email": "eduardo2@email.com"}
 }
+
+class Student(BaseModel): # Creating the schema of the data
+    name: str
+    age: int
+    grade: int
+    email: str
+
+class UpdateStudent(BaseModel):
+    name: Optional[str] = None
+    age: Optional[int] = None
+    grade: Optional[int] = None
+    email: Optional[str] = None
 
 @app.get("/") # Homepage
 def index():
@@ -66,3 +81,34 @@ def get_student(*, student_id: Optional[int] = None, name: str): # If only the n
         elif students[std_id]["name"].lower() == name and student_id is None:
             return students[std_id]
     return {"Data": "Not Found"}
+
+@app.post("/create-student/{student_id}")
+def create_student(student_id: int, student : Student): # student_id behaves like header/path input; student behaves like body
+    if student_id in students:
+        return {"Error": "Student exists"}
+
+    students[student_id] = student.model_dump() # Creates the new student, giving its attributes. I'm using model_dump() to turn it from an object to a dict entry, so it adapts to my previous function
+    return students[student_id] # HTTP response telling the result of the object created
+
+@app.put("/update-student/{student_id}")
+def update_student(student_id: int, student: UpdateStudent): # I utilize a new class, UpdateStudent, because in that way the user won't be forced to update every information, being able now to change optional ones only
+    if student_id not in students:
+        return {"Error": "Student not found"}
+
+    stored_student = students[student_id]
+
+    updated_data = student.model_dump(exclude_unset=True) # Excludes fields with None and only includes fields provided
+
+    for key, value in updated_data.items():
+        stored_student[key] = value # It takes the value linked to the key from the dict created by dumping the object and updates the value while keeping the old values simultaneously
+
+    students[student_id] = stored_student
+    return students[student_id]
+
+@app.delete("/student-delete/{student_id}")
+def student_delete(student_id: int):
+    if student_id not in students:
+        return {"Error": "Student not found"}
+
+    del students[student_id]
+    return {"Message": "Student deleted"}
